daab0a85bd7d35aa8240bd807371d5fc7637fbeb
shonmikl <mikhailegorov676@gmail.com>
2021-11-21 18:48:36 +0300

    Initial commit

---
+++ src/socketor/Phone.java
@@ -0,0 +1,75 @@
+package socketor;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Phone {
+    ServerSocket server;
+    Socket client;
+    BufferedReader reader;
+    BufferedWriter writer;
+
+
+    public Phone(String port) {
+        try {
+            server = new ServerSocket(Integer.parseInt(port));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Phone(String ip, String port) {
+        try {
+            client = new Socket(ip, Integer.parseInt(port));
+            createStreams();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void accept() {
+        try {
+            client = server.accept();
+            createStreams();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createStreams() {
+        try {
+            reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readLine() {
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "-1";
+    }
+
+    public void writeLine(String message) {
+        try {
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            reader.close(); writer.close(); client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/socketor/Socketor.java
@@ -0,0 +1,60 @@
+package socketor;
+
+public class Socketor {
+    public static void main(String[] args) {
+        if(args.length < 3 ) {
+            System.out.println("""
+                    Usage:\r
+                    java socketor.Socketor server 8000 /\r
+                    java socketor.Socketor client 127.0.0.1 8000\r
+                    """);
+            return;
+        }
+        Socketor socketor = new Socketor();
+        if(args[0].equals("server")) {
+            socketor.runServer(args[1], args[2]);
+        } else if(args[0].equals("client")) {
+            socketor.runClient(args[1], args[2], args[3], args[4]);
+        }
+    }
+
+    private void runClient(String ip, String port, String digit1, String digit2) {
+        Phone phone = new Phone(ip, port);
+        phone.writeLine(digit1);
+        phone.writeLine(digit2);
+        String answer = phone.readLine();
+        System.out.println(answer);
+        phone.close();
+    }
+
+    private void runServer(String port, String operation) {
+        Phone  phone = new Phone(port);
+        System.out.println("Server is working on a port " + port + ", operation is " + operation);
+        while (true) {
+            phone.accept();
+            String a = phone.readLine();
+            String b = phone.readLine();
+            double result = calculate(operation, a, b);
+            String message = a + " " + operation + " " + b + " = " + result;
+            phone.writeLine(message);
+            System.out.println("Accepted " + message);
+            phone.close();
+        }
+    }
+
+    private double calculate(String operation, String x, String y) throws ArithmeticException {
+        double results = 0;
+        double a = Double.parseDouble(x);
+        double b = Double.parseDouble(y);
+        switch (operation) {
+            case "*" -> results = a * b;
+            case "/" -> results = a / b;
+            case "-" -> results = a - b;
+            case "+" -> results = a + b;
+            case "^" -> results = Math.pow(a, b);
+            case "%" -> results = a % b;
+            default -> System.out.println("This command isn't correct");
+        }
+        return results;
+    }
+}
